d67e28c3a199d0e80dae355a2a9fd2aaad8f33b9
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-18 14:36:47 +0530

    Add files via upload

--- Practice Code/StarPatterns.java
+++ Practice Code/StarPatterns.java
@@ -75,9 +75,9 @@ public class StarPatterns {
 			}
 			System.out.println("");
 		}
-		for (int i = 3 ; i >= 1; i--) {
+		for (int i = 3 - 1; i >= 1; i--) {
 
-			for (int j = 1; j <= 3- i; j++) {
+			for (int j = 1; j <= 3 - i; j++) {
 				System.out.print(" ");
 			}
 			for (int j = 1; j <= i; j++) {
