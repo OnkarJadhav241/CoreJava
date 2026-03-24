9274b8aa096c4d04cd72d85bd6e1e53e465cedc0
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-24 14:28:52 +0530

    Add files via upload

---
+++ Methods/NumberSquare.java
@@ -0,0 +1,9 @@
+package com.methods;
+
+public class NumberSquare {
+	public void square() {
+		int num = 10;
+		int result = num * num;
+		System.out.println("Square of a " + num + " is " + result);
+	}
+}
