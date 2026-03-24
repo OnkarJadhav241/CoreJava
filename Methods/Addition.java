d20c51fae543424e8356601e0c52ff20f127cac5
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-24 14:27:39 +0530

    Add files via upload

---
+++ Methods/Addition.java
@@ -0,0 +1,11 @@
+package com.methods;
+
+public class Addition {
+	int num1, num2;
+
+	public void addition() {
+		int result = num1 + num2;
+		System.out.println("Addtion of " + num1 + " and " + num2 + " is " + result);
+	}
+
+}
---
+++ Methods/MainClass.java
@@ -0,0 +1,36 @@
+package com.methods;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+//		Addition add = new Addition();
+//		add.num1 = 50;
+//		add.num2 = 40;
+//		add.addition();
+
+//		EvenOrOdd evenorodd =new EvenOrOdd();
+//		evenorodd.checkEvenOrODD();
+//		System.out.println("Even Numbers Series");
+//		evenorodd.printEvenSereies();
+//		
+//		System.out.println("Odd Numbers Series");
+//		evenorodd.printOddSereies();
+
+//		NumberSquare squareno =new NumberSquare();
+//		squareno.square();
+
+//		GreaterNumber greater = new GreaterNumber();
+//		greater.gretestNumber();
+
+//		Fibonacci fibo =new Fibonacci();
+//		fibo.printFibonacciSeries();
+
+//		ReverseNumber rev = new ReverseNumber();
+//		rev.reverse();
+
+		PalindromeNumber pn = new PalindromeNumber();
+		pn.checkplaindrome();
+	}
+
+}
