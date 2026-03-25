98a6e8f8d436edc9d36a33f0aa7753c86dedccbe
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-25 15:03:56 +0530

    Add files via upload

---
+++ Methods :Practice Question/EvenOrOdd.java
@@ -0,0 +1,38 @@
+package com.methods;
+
+public class EvenOrOdd {
+//	public void checkEvenOrOdd() {
+//		int num = 10;
+//		if (num % 2 == 0) {
+//			System.out.println(num + " is Even Number");
+//		} else {
+//			System.out.println(num + " is Odd Number");
+//		}
+//	}
+//	
+//	public void checkEvenOrOdd( int num) {
+//		if (num % 2 == 0) {
+//			System.out.println(num + "is Even Number");
+//		} else {
+//			System.out.println(num + "is Odd Number");
+//		}
+//	}
+	
+//	public String checkEvenOrOdd() {
+//		int num=45;
+//		if (num % 2 == 0) {
+//			return num + "is Even Number";
+//		} else {
+//			return num + "is Odd Number";
+//		}
+//	}
+	
+	public String checkEvenOrOdd(int num) {
+		if (num % 2 == 0) {
+			return num + "is Even Number";
+		} else {
+			return num + "is Odd Number";
+		}
+	}
+
+}
