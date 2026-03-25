011efc8fbce43709db88e7a30249429e2ace7ae1
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-25 15:17:07 +0530

    Add files via upload

---
+++ Method Practice Question/ReverseNumber.java
@@ -0,0 +1,57 @@
+package com.methods;
+
+public class ReverseNumber {
+
+//	public void reverse() {
+//		int num = 12345, reverse = 0;
+//		int actual = num;
+//		while (num > 0) {
+//			int rem = num % 10; // 5
+//			reverse = reverse * 10 + rem; // 0 * 10 +4 =4
+//			num = num / 10; // 12345/10 =1234
+//		}
+//		System.out.println("Actual Number :-" + actual);
+//
+//		System.out.println("Reverse Number:- " + reverse);
+//	}
+
+//	public void reverse(int num) {
+//		int  reverse = 0;
+//		int actual = num;
+//		while (num > 0) {
+//			int rem = num % 10; // 5
+//			reverse = reverse * 10 + rem; // 0 * 10 +4 =4
+//			num = num / 10; // 12345/10 =1234
+//		}
+//		System.out.println("Actual Number :-" + actual);
+//
+//		System.out.println("Reverse Number:- " + reverse);
+//	}
+
+//	public String reverse() {
+//		int num = 12345, reverse = 0;
+//		int actual = num;
+//		while (num > 0) {
+//			int rem = num % 10; // 5
+//			reverse = reverse * 10 + rem; // 0 * 10 +4 =4
+//			num = num / 10; // 12345/10 =1234
+//		}
+//		System.out.println("Actual Number :-" + actual);
+//
+//		return " Reverse Number:- " + reverse;
+//	}
+
+	public String reverse(int num) {
+		int reverse = 0;
+		int actual = num;
+		while (num > 0) {
+			int rem = num % 10; // 5
+			reverse = reverse * 10 + rem; // 0 * 10 +4 =4
+			num = num / 10; // 12345/10 =1234
+		}
+		System.out.println("Actual Number :-" + actual);
+
+		return "Reverse Number:- " + reverse;
+	}
+
+}
