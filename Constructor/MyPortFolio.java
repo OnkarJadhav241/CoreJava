d8a76fd0ca3bf46825886fca71e55bf4c73326c3
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-03-26 14:24:54 +0530

    Add files via upload

---
+++ Constructor/MyPortFolio.java
@@ -0,0 +1,19 @@
+package com.constuctor;
+
+public class MyPortFolio {
+
+	public MyPortFolio(String fname, String lname, int age, String pastQualification, String currentQualification,
+			String email, long mobileNo, double percentage) {
+		System.out.println("--------My Information----------");
+		System.out.println("First Name :- " + fname);
+		System.out.println("Last Name :- " + lname);
+		System.out.println("Age :- " + age);
+		System.out.println("Past Qualification :- " + pastQualification);
+		System.out.println("Current Qualification :- " + currentQualification);
+		System.out.println("Email Id :- " + email);
+		System.out.println("Mobile No :- " + mobileNo);
+		System.out.println("Precentage :- " + percentage + " CGPA");
+
+	}
+
+}
