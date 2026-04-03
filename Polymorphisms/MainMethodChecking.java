3faf0e3a446b5648b6d0de167182010d90675156
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-04-03 15:20:18 +0530

    Add files via upload

---
+++ Polymorphisms/MainMethodChecking.java
@@ -0,0 +1,15 @@
+package com.polymorphism;
+
+public class MainMethodChecking {
+
+	public static void main(String[] args) {
+		System.out.println("Hell world");
+
+	}
+
+	public static void main(String[] args, int a) {
+		System.out.println("Hell world");
+
+	}
+
+}
