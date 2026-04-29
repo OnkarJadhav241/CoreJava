ea2b2a3f5e7f338f843e2725123e2e657018bb6f
OnkarJadhav241 <onkarjadhav2413@gmail.com>
2026-04-29 19:40:37 +0530

    Add files via upload

---
+++ ArrayPractiseSession/Program1.java
@@ -0,0 +1,21 @@
+package arraypractisesession;
+
+public class Program1 {
+
+//	Write a program to declare and initialize a 1D array of integers.
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 20, 30, 40, 50 }; // static array
+
+		int[] arr1 = new int[5];// dynamic array
+
+		arr1[0] = 11;
+		arr1[1] = 22;
+		arr1[2] = 33;
+		arr1[3] = 44;
+		arr1[4] = 55;
+
+	}
+
+}
